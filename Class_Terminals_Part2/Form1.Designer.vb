0adc6d30920d6b26079bc6af8978cef4b0475751
jmcarth4@gmail.com <jmcarth4@gmail.com>
2023-10-05 17:49:15 -0600

    Add project files.

---
+++ Class_Terminals_Part2/Form1.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.InTermListBox = New System.Windows.Forms.ListBox()
+        Me.OutTermListBox = New System.Windows.Forms.ListBox()
+        Me.DataInputClearButton = New System.Windows.Forms.Button()
+        Me.OutClearButton = New System.Windows.Forms.Button()
+        Me.InClearButton = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SendButton = New System.Windows.Forms.Button()
+        Me.PortDataListBox = New System.Windows.Forms.ListBox()
+        Me.PortOpenButton = New System.Windows.Forms.Button()
+        Me.ComPortListBox = New System.Windows.Forms.ListBox()
+        Me.BaudRateListBox = New System.Windows.Forms.ListBox()
+        Me.QuitButton = New System.Windows.Forms.Button()
+        Me.ComPortButton = New System.Windows.Forms.Button()
+        Me.BaudRateButton = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.DataInputLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'InTermListBox
+        '
+        Me.InTermListBox.FormattingEnabled = True
+        Me.InTermListBox.ItemHeight = 20
+        Me.InTermListBox.Location = New System.Drawing.Point(823, 194)
+        Me.InTermListBox.Name = "InTermListBox"
+        Me.InTermListBox.Size = New System.Drawing.Size(325, 244)
+        Me.InTermListBox.TabIndex = 47
+        '
+        'OutTermListBox
+        '
+        Me.OutTermListBox.FormattingEnabled = True
+        Me.OutTermListBox.ItemHeight = 20
+        Me.OutTermListBox.Location = New System.Drawing.Point(581, 194)
+        Me.OutTermListBox.Name = "OutTermListBox"
+        Me.OutTermListBox.Size = New System.Drawing.Size(226, 244)
+        Me.OutTermListBox.TabIndex = 46
+        '
+        'DataInputClearButton
+        '
+        Me.DataInputClearButton.Location = New System.Drawing.Point(782, 12)
+        Me.DataInputClearButton.Name = "DataInputClearButton"
+        Me.DataInputClearButton.Size = New System.Drawing.Size(110, 58)
+        Me.DataInputClearButton.TabIndex = 45
+        Me.DataInputClearButton.Text = "Clear data input "
+        Me.DataInputClearButton.UseVisualStyleBackColor = True
+        '
+        'OutClearButton
+        '
+        Me.OutClearButton.Location = New System.Drawing.Point(611, 444)
+        Me.OutClearButton.Name = "OutClearButton"
+        Me.OutClearButton.Size = New System.Drawing.Size(154, 104)
+        Me.OutClearButton.TabIndex = 44
+        Me.OutClearButton.Text = "Clear data out display"
+        Me.OutClearButton.UseVisualStyleBackColor = True
+        '
+        'InClearButton
+        '
+        Me.InClearButton.Location = New System.Drawing.Point(858, 444)
+        Me.InClearButton.Name = "InClearButton"
+        Me.InClearButton.Size = New System.Drawing.Size(154, 104)
+        Me.InClearButton.TabIndex = 43
+        Me.InClearButton.Text = "clear data in display"
+        Me.InClearButton.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(639, 34)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 26)
+        Me.TextBox1.TabIndex = 42
+        '
+        'SendButton
+        '
+        Me.SendButton.Location = New System.Drawing.Point(622, 80)
+        Me.SendButton.Name = "SendButton"
+        Me.SendButton.Size = New System.Drawing.Size(154, 104)
+        Me.SendButton.TabIndex = 41
+        Me.SendButton.Text = "Send Packet"
+        Me.SendButton.UseVisualStyleBackColor = True
+        '
+        'PortDataListBox
+        '
+        Me.PortDataListBox.FormattingEnabled = True
+        Me.PortDataListBox.ItemHeight = 20
+        Me.PortDataListBox.Location = New System.Drawing.Point(299, 12)
+        Me.PortDataListBox.Name = "PortDataListBox"
+        Me.PortDataListBox.Size = New System.Drawing.Size(173, 204)
+        Me.PortDataListBox.TabIndex = 40
+        '
+        'PortOpenButton
+        '
+        Me.PortOpenButton.Location = New System.Drawing.Point(344, 237)
+        Me.PortOpenButton.Name = "PortOpenButton"
+        Me.PortOpenButton.Size = New System.Drawing.Size(154, 104)
+        Me.PortOpenButton.TabIndex = 39
+        Me.PortOpenButton.Text = "Connect"
+        Me.PortOpenButton.UseVisualStyleBackColor = True
+        '
+        'ComPortListBox
+        '
+        Me.ComPortListBox.FormattingEnabled = True
+        Me.ComPortListBox.ItemHeight = 20
+        Me.ComPortListBox.Location = New System.Drawing.Point(173, 12)
+        Me.ComPortListBox.Name = "ComPortListBox"
+        Me.ComPortListBox.Size = New System.Drawing.Size(110, 184)
+        Me.ComPortListBox.TabIndex = 36
+        '
+        'BaudRateListBox
+        '
+        Me.BaudRateListBox.FormattingEnabled = True
+        Me.BaudRateListBox.ItemHeight = 20
+        Me.BaudRateListBox.Location = New System.Drawing.Point(2, 12)
+        Me.BaudRateListBox.Name = "BaudRateListBox"
+        Me.BaudRateListBox.Size = New System.Drawing.Size(165, 204)
+        Me.BaudRateListBox.TabIndex = 35
+        '
+        'QuitButton
+        '
+        Me.QuitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.QuitButton.Location = New System.Drawing.Point(1055, 546)
+        Me.QuitButton.Name = "QuitButton"
+        Me.QuitButton.Size = New System.Drawing.Size(136, 104)
+        Me.QuitButton.TabIndex = 34
+        Me.QuitButton.Text = "Quit"
+        Me.QuitButton.UseVisualStyleBackColor = True
+        '
+        'ComPortButton
+        '
+        Me.ComPortButton.Location = New System.Drawing.Point(173, 237)
+        Me.ComPortButton.Name = "ComPortButton"
+        Me.ComPortButton.Size = New System.Drawing.Size(154, 104)
+        Me.ComPortButton.TabIndex = 33
+        Me.ComPortButton.Text = "Com Port"
+        Me.ComPortButton.UseVisualStyleBackColor = True
+        '
+        'BaudRateButton
+        '
+        Me.BaudRateButton.Location = New System.Drawing.Point(24, 242)
+        Me.BaudRateButton.Name = "BaudRateButton"
+        Me.BaudRateButton.Size = New System.Drawing.Size(127, 95)
+        Me.BaudRateButton.TabIndex = 32
+        Me.BaudRateButton.Text = "Baud Rate"
+        Me.BaudRateButton.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'DataInputLabel
+        '
+        Me.DataInputLabel.AutoSize = True
+        Me.DataInputLabel.Location = New System.Drawing.Point(660, 9)
+        Me.DataInputLabel.Name = "DataInputLabel"
+        Me.DataInputLabel.Size = New System.Drawing.Size(85, 20)
+        Me.DataInputLabel.TabIndex = 48
+        Me.DataInputLabel.Text = "Data Input"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1222, 692)
+        Me.Controls.Add(Me.DataInputLabel)
+        Me.Controls.Add(Me.InTermListBox)
+        Me.Controls.Add(Me.OutTermListBox)
+        Me.Controls.Add(Me.DataInputClearButton)
+        Me.Controls.Add(Me.OutClearButton)
+        Me.Controls.Add(Me.InClearButton)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.SendButton)
+        Me.Controls.Add(Me.PortDataListBox)
+        Me.Controls.Add(Me.PortOpenButton)
+        Me.Controls.Add(Me.ComPortListBox)
+        Me.Controls.Add(Me.BaudRateListBox)
+        Me.Controls.Add(Me.QuitButton)
+        Me.Controls.Add(Me.ComPortButton)
+        Me.Controls.Add(Me.BaudRateButton)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents InTermListBox As ListBox
+    Friend WithEvents OutTermListBox As ListBox
+    Friend WithEvents DataInputClearButton As Button
+    Friend WithEvents OutClearButton As Button
+    Friend WithEvents InClearButton As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents SendButton As Button
+    Friend WithEvents PortDataListBox As ListBox
+    Friend WithEvents PortOpenButton As Button
+    Friend WithEvents ComPortListBox As ListBox
+    Friend WithEvents BaudRateListBox As ListBox
+    Friend WithEvents QuitButton As Button
+    Friend WithEvents ComPortButton As Button
+    Friend WithEvents BaudRateButton As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents DataInputLabel As Label
+End Class
